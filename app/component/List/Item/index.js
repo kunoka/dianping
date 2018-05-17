093dee85f429a2f91c2b9373a23fd4e53d2e8b1f
Harry <hengwang@cn.ibm.com>
2018-05-17 22:58:59 +0800

    首页开发18-猜你喜欢-显示内容 结束

--- app/component/List/Item/index.js
+++ app/component/List/Item/index.js
@@ -9,7 +9,24 @@ export default class Item extends React.Component {
   render() {
     const data = this.props.data;
     return (
-      <p>{data.title}</p>
+      <div className="list-item">
+        <div className="item-img-container">
+          <img src={data.img} alt={data.title}/>
+        </div>
+        <div className="item-content">
+          <div className="item-title-container">
+            <div className="title">{data.title}</div>
+            <div className="distance">{data.distance}</div>
+          </div>
+          <div className="item-subtitle">{data.subTitle}</div>
+          <div className="item-price-container">
+            <div className="price">￥{data.price}</div>
+            <div className="number">已售{data.mumber}</div>
+          </div>
+
+        </div>
+      </div>
+
     )
   }
 }
